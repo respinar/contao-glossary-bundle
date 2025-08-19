36d2c9b06cf13f8d560fb3d592a7afe51113bd96
Hamid Peywasti <hamid.peywasti@gmail.com>
2025-08-19 23:30:27 +0330

    Convert to Content Element Controller

---
+++ src/Controller/ContentElement/GlossaryController.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of Contao Simple Glossary.
+ *
+ * (c) Hamid Peywasti 2024 <hamid@respinar.com>
+ *
+ * @license MIT
+ */
+
+namespace Respinar\GlossaryBundle\Controller\ContentElement;
+
+use Contao\ContentModel;
+use Contao\CoreBundle\Controller\ContentElement\AbstractContentElementController;
+use Contao\CoreBundle\DependencyInjection\Attribute\AsContentElement;
+use Contao\CoreBundle\Twig\FragmentTemplate;
+use Respinar\GlossaryBundle\Model\GlossaryTermModel;
+use Respinar\GlossaryBundle\Renderer\GlossaryTermRenderer;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+#[AsContentElement( type: GlossaryController::TYPE, category: 'miscellaneous')]
+final class GlossaryController extends AbstractContentElementController
+{
+    public const TYPE = 'glossary';
+
+    public function __construct(
+        private readonly GlossaryTermRenderer $renderer,
+    ) {
+    }
+
+    protected function getResponse(FragmentTemplate $template, ContentModel $model, Request $request): Response {
+        $t = GlossaryTermModel::getTable();
+
+        $arrOptions = match ($model->glossary_term_order) {
+            'order_term_desc' => [
+                'order' => "$t.term DESC",
+            ],
+            default => [
+                'order' => "$t.term",
+            ],
+        };
+
+        $objTerms = GlossaryTermModel::findBy(
+            'pid',
+            $model->glossary,
+            $arrOptions
+        );
+
+        $template->set(
+            'arrElements',
+            $this->renderer->render($objTerms, $model)
+        );
+
+        return $template->getResponse();
+    }
+}
